4e5d6b814c503ef4bf22a24145aa777214543ecd
John Douglas Price <johndouglasprice@Johns-MacBook-Pro.local>
2021-04-14 11:02:26 -0400

    Translated C# to JS

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,23 @@
 // Put your code here
+let names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+"London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
+
+console.log("All Place Names")
+
+// Iterate over each "name" in the "names" list
+names.forEach(name => {
+    console.log(name)
+})
+
+console.log("")
+let theNames = []
+  names.filter(name => {
+    if(name.startsWith("The")){
+        theNames.push(name)
+    }
+})
+
+console.log("'The' Place Names")
+theNames.forEach(name => {
+    console.log(name)
+})
